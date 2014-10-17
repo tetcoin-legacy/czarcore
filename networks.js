11485eb4563246bfeaf290513d60310e3a94de7b
Treefunder Incorporated <owner@czarcoin.co>
2014-10-17 14:18:10 +0000

    Czarcoin Factoring

--- networks.js
+++ networks.js
@@ -4,7 +4,9 @@ var hex = function(hex) {
   return new Buffer(hex, 'hex');
 };
 
-/* rememeber hash is backwards 5174015f0852af7f62c7f92bd5816facb31580706282580c2b6d82b815aba2de*/
+/* rememeber hash is backwards */
+/* Livenet: 26386e3d1dfe042576122eef381d662551f33d68eb29e3a3fb5fd0cd5f25788e */
+/* Testnet: bb43407716677ded9c2c32cf82ecb3c26b2d24a552dbcb4d53257a9f4b32178a */
 exports.livenet = {
   name: 'livenet',
   magic: hex('637a6172'),
@@ -14,10 +16,10 @@ exports.livenet = {
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('dea2ab15b8826d2b0c588262708015b3ac6f81d52bf9c7627faf52085f017451'),
-    merkle_root: hex('340651462c6b2d44f29d5a15af76fa6a1acaee6c7b9d15d1183609a26f9e9548'),
+    hash: hex('8e78255fcdd05ffba3e329eb683df35125661d38ef2e12762504fe1d3d6e3826'),
+    merkle_root: hex('adc2a5fbf9b0d5ab9b34fadc992d0d3666907743147b4db694b4218cee5eda13'),
     height: 0,
-    nonce: 2085290533,
+    nonce: 2084732958,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
     timestamp: 1413169227,
@@ -41,13 +43,13 @@ exports.testnet = {
   hkeyPublicVersion: 0x043587cf,
   hkeyPrivateVersion: 0x04358394,
   genesisBlock: {
-    hash: hex('01e335ca6f51c3d9680f551d21923fb68880e36ec86ea1575f57394aabbfc572'),
-    merkle_root: hex('340651462c6b2d44f29d5a15af76fa6a1acaee6c7b9d15d1183609a26f9e9548'),
+    hash: hex('8a17324b9f7a25534dcbdb52a5242d6bc2b3ec82cf322c9ced7d6716774043bb'),
+    merkle_root: hex('adc2a5fbf9b0d5ab9b34fadc992d0d3666907743147b4db694b4218cee5eda13'),
     height: 0,
-    nonce: 385520184,
+    nonce: 386799647,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1296688602,
+    timestamp: 1413169260,
     bits: 486604799,
   },
   dnsSeeds: [
